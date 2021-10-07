e49ad32a8542d3dfae0f1f5d7b7a29dd851035c4
Sylvain Leclercq <sylvain.leclercq@external.thalesgroup.com>
2021-10-07 13:49:14 +0000

    Adds stuff

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,76 @@
-import { commands, CompleteResult, ExtensionContext, listManager, sources, window, workspace } from 'coc.nvim';
-import DemoList from './lists';
+import { commands, CompleteResult, ExtensionContext, LanguageClient, LanguageClientOptions, services, window, workspace } from 'coc.nvim';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  window.showMessage(`coc-als works!`);
 
+  window.showMessage ("Configuration: ");
+  let options = workspace.getConfiguration("ada");
+  window.showMessage (JSON.stringify(options));
+
+  const serverOptions = {
+    command: 'ada_language_server',
+  }
+  const clientOption : LanguageClientOptions = {
+    documentSelector: ['ada']
+  }
+  const client = new LanguageClient(
+    'coc-als',
+    'ada_language_server',
+    serverOptions,
+    clientOption
+  )
+  // window.showMessage(`coc-als works!`);
+  // function otherFileHandler() {
+      // const activeEditor = window.;
+      // if (!activeEditor) {
+          // return;
+      // }
+      // void client.sendRequest(languageclient.ExecuteCommandRequest.type, {
+          // command: 'als-other-file',
+          // arguments: [
+              // {
+                  // uri: window.document.uri.toString(),
+              // },
+          // ],
+      // });
+  // }
+
+  context.subscriptions.push(services.registLanguageClient(client))
   context.subscriptions.push(
-    commands.registerCommand('coc-als.Command', async () => {
-      window.showMessage(`coc-als Commands works!`);
+    commands.registerCommand('ada.otherFile', async () => {
+      let document = await workspace.document;
+      let file = document.uri
+
+      window.showMessage(`coc-als Commands works! ${file}`);
+      workspace.loadFile(`${file}.tmp`);
     }),
 
-    listManager.registerList(new DemoList(workspace.nvim)),
+    // listManager.registerList(new DemoList(workspace.nvim)),
 
-    sources.createSource({
-      name: 'coc-als completion source', // unique id
-      doComplete: async () => {
-        const items = await getCompletionItems();
-        return items;
-      },
-    }),
+    // sources.createSource({
+      // name: 'coc-als completion source', // unique id
+      // doComplete: async () => {
+        // const items = await getCompletionItems();
+        // return items;
+      // },
+    // }),
 
-    workspace.registerKeymap(
-      ['n'],
-      'als-keymap',
-      async () => {
-        window.showMessage(`registerKeymap`);
-      },
-      { sync: false }
-    ),
+    // workspace.registerKeymap(
+      // ['n'],
+      // 'als-keymap',
+      // async () => {
+        // window.showMessage(`registerKeymap`);
+      // },
+      // { sync: false }
+    // ),
 
-    workspace.registerAutocmd({
-      event: 'InsertLeave',
-      request: true,
-      callback: () => {
-        window.showMessage(`registerAutocmd on InsertLeave`);
-      },
-    })
+    // workspace.registerAutocmd({
+      // event: 'InsertLeave',
+      // request: true,
+      // callback: () => {
+        // window.showMessage(`registerAutocmd on InsertLeave`);
+      // },
+    // })
   );
 }
 
-async function getCompletionItems(): Promise<CompleteResult> {
-  return {
-    items: [
-      {
-        word: 'TestCompletionItem 1',
-        menu: '[coc-als]',
-      },
-      {
-        word: 'TestCompletionItem 2',
-        menu: '[coc-als]',
-      },
-    ],
-  };
-}
+
